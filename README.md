a0b0367687443af62431b74530cf27373e1f1b9a
Charles Villalpando <charlesvill@g.ucla.edu>
2023-10-06 13:41:33 -0700

    Refactored function as ES6 module & updated README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Image-carousel
 Minimalist image slider carousel style with navigation indicator dots
+
+### Implementation: 
+- The following need to be present in the HTML prior to this script running: 
+	1. data-index dataset on each of the circle navigator buttons
+	2. 4 images with the same size dimensions on a 1:1 aspect ratio
+		- Any desire to change this will need to mess with the CSS file
+- To initialize the carousel: 
+	1. import 'initializeImgCarousel' from './path'
+	2. Assign const variable to the function passing through the following in this order: 
+		- className for circle buttons (they all have to share one className)
+		- className for the slide container (that houses the photos)
+		- className for images (again, need to share one className)
+		- Size of the images (hypothetically if they're all atleast the same width, you should be fine)
+	3. You should be good to go!
--- index.js
+++ index.js
@@ -1,80 +1,78 @@
 
-const navBtns = document.querySelectorAll(".navBtn");
-const sliderCont = document.querySelector(".slider");
-const images = document.querySelectorAll(".photo");
-const pictureSize = 480;
-let slideIndex = 0;
+export default function initializeImgCarousel(NavBtnCN, sliderCN, imagesCN, imgSize)
+{
+    const navBtns = document.querySelectorAll(`.${NavBtnCN}`);
+    const sliderCont = document.querySelector(`.${sliderCN}`);
+    const images = document.querySelectorAll(`.${imagesCN}`);
 
-navBtns.forEach(element => element.addEventListener("click", function(e){
-    e.preventDefault();
-    scrollToFixed(e);
-}))
+    let slideIndex = 0;
 
-document.addEventListener("keydown", (e)=>{
-    const keyPress = e.key;
-    let direction;
-    // scroll right (-1)
-    // scroll left (1)
+    navBtns.forEach(element => element.addEventListener("click", function(e){
+        e.preventDefault();
+        scrollToFixed(e);
+    }))
 
-    if (keyPress === 'ArrowLeft'){
-        direction = -1;
+    document.addEventListener("keydown", (e)=>{
+        const keyPress = e.key;
+        let direction;
+        // scroll right (1)
+        // scroll left (-1)
+
+        if (keyPress === 'ArrowLeft'){
+            direction = -1;
+        }
+        else if (keyPress === 'ArrowRight'){
+            direction = 1;
+        }
+        scrollRelative(direction);
+    })
+
+    function scrollToFixed(e){
+       const dataIndex = e.currentTarget.getAttribute("data-index");
+       sliderCont.removeAttribute('style');
+
+       switch(dataIndex)
+       {
+        case '1':
+            classChange("photoOne");
+            break;
+        case '2':
+            classChange("photoTwo");
+            break;
+        case '3':
+            classChange("photoThree");
+            break;
+        case '4':
+            classChange("photoFour");
+            break;
+        default:
+            alert("Something wrong with the class changer");
+       }
+
+       function classChange(className){
+        sliderCont.classList.remove(sliderCont.classList[1]);
+        sliderCont.classList.add(className);
+       }
     }
-    else if (keyPress === 'ArrowRight'){
-        direction = 1;
-    }
-    scrollRelative(direction);
-})
 
-function scrollToFixed(e){
-   const dataIndex = e.currentTarget.getAttribute("data-index");
-   sliderCont.removeAttribute('style');
+    function scrollRelative(direction){
+        slideIndex += direction;
 
-   switch(dataIndex)
-   {
-    case '1':
-        classChange("photoOne");
-        break;
-    case '2':
-        classChange("photoTwo");
-        break;
-    case '3':
-        classChange("photoThree");
-        break;
-    case '4':
-        classChange("photoFour");
-        break;
-    default:
-        alert("Something wrong with the class changer");
-   }
-
-   function classChange(className){
-    sliderCont.classList.remove(sliderCont.classList[1]);
-    sliderCont.classList.add(className);
-    console.log(`the class ${className} is being added!`);
-   }
-}
-
-function scrollRelative(direction){
-    slideIndex += direction;
-
-   // slideIndex is only purpose is to prevent going beyond the bounds of available images
-
-
-    if(slideIndex < 0){
-        slideIndex = images.length - 1;
-        calculateTransform(slideIndex);
-    }
-    else if(slideIndex >= images.length){
-        slideIndex = 0;
-        calculateTransform(slideIndex);
-    }
-    else{
-        calculateTransform(slideIndex);
-    }
-    function calculateTransform(shiftPos){
-        console.log(slideIndex);
-        // multiplies the slideIndex by the photo size
-        const translateX = shiftPos * pictureSize;
-        sliderCont.style.transform = `translateX(-${translateX}px)`;
+        if(slideIndex < 0){
+            slideIndex = images.length - 1;
+            calculateTransform(slideIndex);
+        }
+        else if(slideIndex >= images.length){
+            slideIndex = 0;
+            calculateTransform(slideIndex);
+        }
+        else{
+            calculateTransform(slideIndex);
+        }
+        function calculateTransform(shiftPos){
+            // multiplies the slideIndex by the photo size
+            const translateX = shiftPos * imgSize;
+            sliderCont.style.transform = `translateX(-${translateX}px)`;
+        }
     }
 }
